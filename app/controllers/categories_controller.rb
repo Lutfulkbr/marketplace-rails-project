0def620f8e4b4dac5cd75c70d54153b28a313186
github username <github email address>
2021-05-03 14:10:32 -0400

    update admin functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,9 +22,9 @@ class CategoriesController < ApplicationController
     end
 
     def update
-        category = Category.find(params[:id])
-        if category
-            category.update(category_params)
+        @category = Category.find(params[:id])
+        if @category
+            @category.update(category_params)
             redirect_to root_path
         else
             render :edit
@@ -32,9 +32,11 @@ class CategoriesController < ApplicationController
     end
 
     def destroy
-        category = Category.find(params[:id])
-        category.destroy
-        redirect_to root_path
+        @category = Category.find(params[:id])
+        if @category
+            @category.destroy
+            redirect_to root_path
+        end
     end
 
     private
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,42 @@ class ProductsController < ApplicationController
         @products = @category.products.order(:title)
     end
 
-    def edit
+    def new
         if params[:category_id]
-            @product = Product.find(params[:id])
+            @category = Category.find_by(id: params[:category_id])
+            @product = @category.products.build
+        end
+    end
+
+    def create
+            @category = Category.find_by(id: params[:category_id])
+            @product = @category.products.build(product_params)
+            if @product.save
+                redirect_to category_products_path(@category)
+            else
+                render :new
+            end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        product = Product.find(params[:id])
+        if product
+            product.update(product_params)
+            redirect_to category_products_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        product = Product.find(params[:id])
+        if product
+            product.destroy
+            redirect_to category_products_path
         end
     end
 
@@ -26,6 +59,10 @@ class ProductsController < ApplicationController
     end
 
     private
+
+    def product_params
+        params.require(:product).permit(:title, :description, :price, :category_ids)
+    end
 
     def cart_session
         session[:cart] || []
